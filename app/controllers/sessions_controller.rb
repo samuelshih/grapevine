f3adccc1935e001b201ebd5c2a3e65559fcd6b1f
Samuel Shih <sam00429@gmail.com>
2015-03-27 17:27:35 -0700

    header to respond to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # log in
+      log_in user
+      redirect_to user
     else
       # Error messages
       flash.now[:danger] = 'Invalid email/password confirmation'
