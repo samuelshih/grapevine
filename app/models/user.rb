0cf1260a208b69b3eb7e20011166162b625e0e37
Samuel Shih <sam00429@gmail.com>
2015-04-21 15:09:22 -0700

    must be logged in for update and edit actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  # has_secure_password enforces presence of password
 
   before_save { self.email = email.downcase }
 
