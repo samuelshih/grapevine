424c81f39e1af5e4fad57a4287a16ca42166fa83
Samuel Shih <sam00429@gmail.com>
2015-04-21 15:58:45 -0700

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
     # Confirms a logged in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = 'Please log in.'
         redirect_to login_url
       end
@@ -52,7 +53,7 @@ class UsersController < ApplicationController
 
     # Confirms the correct user
     def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -35,4 +35,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal @user.name, name
     assert_equal @user.email, email
   end
+
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+
+    name = 'foo bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: '',
+                                    password_confirmation: ''
+                          }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
 end
