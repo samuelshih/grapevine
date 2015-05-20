79b8e6d3e7313a6054c1b2d4e6a50ca8d55bfc64
Samuel Shih <sam00429@gmail.com>
2015-05-19 17:08:36 -0700

    password resets all done locally

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
 
   # Sets the password reset attributes
   def create_reset_digest
-    self.reset_token = User.new_Token
+    self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
@@ -65,6 +65,11 @@ class User < ActiveRecord::Base
   # Sends password reset email
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
