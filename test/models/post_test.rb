f969edb2aa3d5d6e20ca2cc2c1120670ae9b1a9b
Samuel Shih <sam00429@gmail.com>
2015-05-21 15:53:19 -0700

    post migration, model relations

---
+++ test/models/post_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:login_test)
+    # This is not idiomatically correct
+    @post = @user.posts.build(content: 'Lorem ipsum')
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
+
+  test 'user id should be present' do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test 'content should be present' do
+    @post.content = ''
+    assert_not @post.valid?
+  end
+
+  test 'content should be at most 140 characters' do
+    @post.content = 'a' * 141
+    assert_not @post.valid?
+  end
+end
