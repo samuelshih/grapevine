6b7fd021f92381795d87f021a2f629f54919a821
Samuel Shih <sam00429@gmail.com>
2015-06-24 16:46:44 -0700

    carrierwave for image uploading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:content, :picture)
     end
 
     def correct_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
 
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+
+  private
+    # Validate the size of an uploaded picture
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, 'should be less than 5MB')
+      end
+    end
 end
