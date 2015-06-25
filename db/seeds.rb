629c42e8cc0009b29cb4934c99abc0250c0aaf86
Samuel Shih <sam00429@gmail.com>
2015-06-25 16:41:01 -0700

    seed db with relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Hacker.say_something_smart # ayy lmao
   users.each { |user| user.posts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
